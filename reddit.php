1c632ee73161ae0772bc4230b501478b3de96ce5
dscholten <dscholten@dscholten-ThinkPad-Edge>
2015-02-01 22:01:20 -0500

    Fixed the minute(s) variable.

--- reddit.php
+++ reddit.php
@@ -17,13 +17,23 @@ Menu reloads page on change.
 <?php
 
 	$i_reloadtime = $_POST['reload_time'];
-echo $i_reloadtime;
-echo "<br>";
-	$out = "Reload Every";
+
+	if ($i_reloadtime <= 1)
+		{
+			$i_reloadtime = 1;
+			$s_min = "minute";
+		}
+	else
+		{
+		$s_min = "minutes";
+		}
+
+	$out = "Reloading every&nbsp".$i_reloadtime."&nbsp".$s_min;
 
 
 	$out .= "<form action='reddit.php' method='post'>
 			<select id='reload_time' name='reload_time' onchange='this.form.submit()'>
+			<option selected value='0'>Change Time</option>
 			<option value='1'>1 Minute</option>
 			<option value='5'>5 Minutes</option>
 			<option value='10'>10 Minutes</option>
